76f6fea081547507c04cbeb567967759ca8a12a6
nandini-chakali <chakalinandini31@gmail.com>
2025-01-28 17:13:23 +0530

    Add files via upload

---
+++ Shiftoperators.java
@@ -0,0 +1,8 @@
+public class Shiftoperators {
+    public static void main(String[] args) {
+        int number = 8;
+        System.out.println("Left shift (<< 1): " + (number << 1));
+        System.out.println("Right shift (>> 1): " + (number >> 1));
+    }
+}
+
